7d896aa0464fc95d69a8739e7b25e6166e51c64f
KING-CV <chiragsingh66636@gmail.com>
2022-04-01 21:58:02 -0400

    its your boi chirag

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       //yo ma g - Chirag 
     }
 
     // Update is called once per frame
