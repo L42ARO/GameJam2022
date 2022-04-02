aa60a81e4c95ac95a495781fd9af214a8bbc9681
KING-CV <chiragsingh66636@gmail.com>
2022-04-01 22:03:11 -0400

    THATS ME IN CAPS

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       //yo ma g - Chirag 
+       //yo ma g - Chirag - I AM BACK IN CAPS
     }
 
     // Update is called once per frame
