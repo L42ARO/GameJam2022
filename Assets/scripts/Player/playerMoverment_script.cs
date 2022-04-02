65a130c2060743d4fd08ce1255ba19b3fa96c8a2
KING-CV <chiragsingh66636@gmail.com>
2022-04-02 02:07:09 -0400

    added gravity

--- Assets/scripts/Player/playerMoverment_script.cs
+++ Assets/scripts/Player/playerMoverment_script.cs
@@ -7,6 +7,9 @@ public class playerMoverment_script : MonoBehaviour
     public CharacterController controller;
     
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
 
     // Start is called before the first frame update
    
@@ -20,6 +23,9 @@ public class playerMoverment_script : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
